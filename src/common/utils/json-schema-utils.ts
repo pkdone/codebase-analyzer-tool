f864481eaf820443f2d8e48b2983e89f3b7ed6b1
Paul Done <pkdone@gmail.com>
2025-07-28 09:25:03 +0100

    refactorings

--- src/common/utils/json-schema-utils.ts
+++ src/common/utils/json-schema-utils.ts
@@ -1,14 +1,15 @@
 import { ZodType, ZodTypeDef } from "zod";
-import { zodToJsonSchema, Options } from "zod-to-json-schema";
+import { zodToJsonSchema, Options, JsonSchema7Type } from "zod-to-json-schema";
 
+/**
+ * Converts a Zod schema to a JSON schema. Removes the $schema property which technologies like 
+ * MongoDB and VertexAI don't support
+ */
 export function zodToJsonSchemaNormalized(
   schema: ZodType<unknown, ZodTypeDef, unknown>,
   options?: string | Partial<Options>,
-) {
-  // Generate the JSON schema
+): Omit<JsonSchema7Type, "$schema"> {
   const jsonSchema = zodToJsonSchema(schema, options);
-
-  // Remove the $schema property which technologies like MongoDB and VertexAI don't support
   const { $schema, ...remainingSchema } = jsonSchema;
   void $schema; // Avoid linting error
   return remainingSchema;
