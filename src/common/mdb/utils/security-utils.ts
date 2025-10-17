f3420ef0deb416e562609ff4771f37e399d9df3c
Paul Done <pkdone@gmail.com>
2025-10-17 11:29:15 +0100

    refactorings

--- src/common/mdb/utils/security-utils.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Deprecated: use `src/common/security/url-redactor.ts` instead.
- * This file now re-exports the generic implementation for backward compatibility.
- */
-export { redactUrl } from "../../security/url-redactor";
