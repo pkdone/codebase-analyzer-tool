2788586eb904e38db6a2bf6f963bfe13aa959652
Paul Done <pkdone@gmail.com>
2025-10-28 19:54:51 +0000

    refactorings

--- src/prompts/templates/master.prompt.ts
+++ src/prompts/templates/master.prompt.ts
@@ -3,7 +3,9 @@
  * This centralizes the common structure for all LLM prompts in the application.
  */
 
-export const MASTER_PROMPT_TEMPLATE = `{{instructions}}
+export const MASTER_PROMPT_TEMPLATE = `Act as a senior developer analyzing the code in a legacy application. Based on the {{contentDesc}} shown below in the section marked 'CODE', return a JSON response that contains:
+
+{{instructions}}.
 
 The JSON response must follow this JSON schema:
 \`\`\`json
@@ -12,6 +14,6 @@ The JSON response must follow this JSON schema:
 
 {{forceJSON}}
 
-CONTENT:
+CODE:
 {{content}}`;
 
